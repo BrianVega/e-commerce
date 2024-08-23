29bb5762e7dacd404985b830316e7de72fcf8600
Brian Vega <BrianVega01@outlook.es>
2024-08-23 17:24:19 -0600

    Refactoring DI into Order model

--- src/main/java/org/ecommerce/models/Order.java
+++ src/main/java/org/ecommerce/models/Order.java
@@ -13,17 +13,19 @@ public class Order extends Identity {
     private Long customerId;
     private Date orderDate;
     private String status;
-    private Currency totalAmount;
-    private String shippingAddress;
     private List<Product> products;
+    private ShippingInformation shippingInformation;
+    private BillingInformation billingInformation;
+    private PaymentDetails paymentDetails;
 
-    Order(Long id, Long customerId, Date orderDate, String status, Currency totalAmount, String shippingAddress, List<Product> products) {
+    Order(Long id, Long customerId, Date orderDate, String status, ShippingInformation shippingInformation, BillingInformation billingInformation, PaymentDetails paymentDetails, List<Product> products) {
         super(id);
         this.customerId = customerId;
         this.orderDate = orderDate;
         this.status = status;
-        this.totalAmount = totalAmount;
-        this.shippingAddress = shippingAddress;
+        this.shippingInformation = shippingInformation;
+        this.billingInformation = billingInformation;
+        this.paymentDetails = paymentDetails;
         this.products = products;
     }
 
